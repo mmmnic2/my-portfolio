c230b36362ea233f21ed723ec7168dafbcec58c6
mmmnic2 <30kfs21@gmail.com>
2025-02-13 21:11:27 +0700

    update working experience

--- components/Experience.jsx
+++ components/Experience.jsx
@@ -4,6 +4,73 @@ export const Experience = () => {
   return (
     <section id="experience" className="relative text-white pt-[90px]">
       <h3 className="text-center font-extrabold text-4xl mb-6">Experience</h3>
+      {/* Toast Canvas VN */}
+      <div className="flex gap-3">
+        <div className="text-end">
+          <p>10/2024 - current <span className="invisible">1</span></p>
+          <span>3 months</span>
+        </div>
+        <div className="flex flex-col items-center gap-1">
+          <div className="shadow-custom brightness-110 bg-primary rounded-[50%] w-5 h-5"></div>
+          <div className="w-[2px] bg-white h-full"></div>
+        </div>
+        <div>
+          <h3 className="text-2xl font-bold">Contractor Backend Developer</h3>
+          <h4 className="text-xl text-secondary-white font-bold">
+            Toast Canvas VN
+          </h4>
+          <div>
+            <span className="font-semibold">Decription</span>
+            <p>
+              A website for managing comic book viewing, including user
+              interaction features such as liking webtoon and commenting on
+              chapters. It also allows authors to publish and manage their own
+              webtoon.
+            </p>
+          </div>
+          <div className="my-4">
+            <span className="font-semibold">Responsibilities</span>
+            <span className="block">Backend:</span>
+            <ul className="text-sm list-[disc] custom-margin-li">
+              <li>
+                Integrated <b>Redis</b> for <b>caching, TTL-based session management, and
+                Token Blacklisting</b>, reducing database load and enhancing
+                security. Ensures <b>2x faster queries</b> and <b>real-time token
+                revocation</b> to prevent unauthorized access after logout.
+              </li>
+              <li>
+                Scheduled a <b>24-hour ranking job</b> in <b>Spring Boot</b> with Pessimistic
+                Locking to ensure data consistency by preventing concurrent
+                updates. This guarantees accurate rankings and database
+                integrity without performance loss.
+              </li>
+              <li>
+                Using <b>MyBatis</b> with <b>XML-based configuration</b> ensures flexible and
+                maintainable SQL management for complex queries. It optimizes
+                performance when <b>loading webtoon chapters, searching authors,
+                and categorizing webtoons</b>, making it more efficient than
+                Hibernate.
+              </li>
+              <li>
+                Developed and maintained RESTful APIs to facilitate data
+                interaction between the front end and back end.
+              </li>
+            </ul>
+          </div>
+          <div>
+            <span className="font-semibold">Technologies</span>
+            <ul className="text-sm list-[disc] custom-margin-li mb-4">
+              <li>
+                Spring Boot, Spring Security, Spring Data JPA, MariaDB, Heidisql
+                IDE, Redis, MyBatis.
+              </li>
+              <li>Docker.</li>
+              <li>Git, GitHub.</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      {/* TMA */}
       <div className="flex gap-3">
         <div className="text-end">
           <p>4/2024 - 7/2024</p>
